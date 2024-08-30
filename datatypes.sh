efb0227221b81674a226e8374c6e2b284483c63f
udayreddyg <golamariudaykiran@gmail.com>
2024-08-30 19:00:44 -0400

    adding two numbers

--- datatypes.sh
+++ datatypes.sh
@@ -4,4 +4,4 @@ echo "enter number one:"
 read first
 echo "enter number two:"
 read second
-echo "the sum of $first and $second is:($first+$second) "
+echo "the sum of $first and $second is:$(($first+$second)) "
