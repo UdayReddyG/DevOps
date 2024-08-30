8f4f0a9bd0a9e82cc0dbff3c6bf5b2ecd0563d7f
udayreddyg <golamariudaykiran@gmail.com>
2024-08-29 22:07:05 -0400

    3practice

--- conversations.sh
+++ conversations.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo"ramesh:hi h r u"
-echo"suresh:iam good wau"
-echo"ramesh:cool whats the josh"
-echo"suresh: shell scripting"
+echo "ramesh:hi h r u"
+echo "suresh:iam good wau"
+echo "ramesh:cool whats the josh"
+echo "suresh: shell scripting"
