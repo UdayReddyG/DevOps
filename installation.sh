0b8975f29ac6ea2db92c98f8e4cee9b69e43cd75
udayreddyg <golamariudaykiran@gmail.com>
2024-08-30 22:03:59 -0400

    condition

---
+++ installation.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+userid=$(id -u)
+validate(){
+    if [ $1 ne 0 ]
+    then
+        echo "$2 not installed"
+        exit 1
+    else
+        echo "$2 is"
+}
+
+if [ $userid ne 0 ]
+then
+    echo "please run with the root privilages"
+    exit 1
+fi
+dnf list installed git
+
+if [ $? ne 0 ]
+then 
+    echo "git not installed"
+    dnf install git
+    validate $? "git"
+else 
+    echo "already installed"
+fi
